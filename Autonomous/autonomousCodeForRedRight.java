c1dbc4121962fadb0e93694a1f01ef7ed02eea5e
Jacøb <85955074+cuber1515@users.noreply.github.com>
2021-12-09 07:02:22 -0700

    Rough Draft.
    This is basically a rough draft for autonomous.
    * Places pre-loaded box in 1st level of the shipping hub.
    * Delivers duck to floor.
    * Parks in warehouse.

--- Autonomous/autonomousCodeForRedRight.java
+++ Autonomous/autonomousCodeForRedRight.java
@@ -187,8 +187,30 @@ public class autonomousCodeForRedRight extends LinearOpMode {
          * Park completely in warehouse
          */
 
-        encoders.calculateToPlace(48, 1338, 5);
+        int Target = encoders.calculateToPlace(48, 1338, 5);
+        encoders(Target);
 
         turn(500,"left");
+
+        wind("up", 1000);
+
+//        drop();
+
+        turn(500, "left");
+
+        Target = encoders.calculateToPlace(45, 1338, 5);
+        encoders(Target);
+
+        turn(500,"right");
+
+        Target = encoders.calculateToPlace(100, 1338, 5);
+        encoders(Target);
+
+        spinDuck(1000);
+        
+        turn(1000, "right");
+        
+        Target = encoders.calculateToPlace(150, 1338, 5);
+        encoders(Target);
     }
 }
