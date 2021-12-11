47ae68c07188893f14abfd109f09b7ffd9810159
Jacøb <85955074+cuber1515@users.noreply.github.com>
2021-12-10 22:26:22 -0700

    Clean up - 1

--- Autonomous/autonomousCodeForBlueLeft.java
+++ Autonomous/autonomousCodeForBlueLeft.java
@@ -46,15 +46,15 @@ public class autonomousCodeForBlueLeft extends LinearOpMode {
     // turn
     public void turn(int tIme, String direction) {
         if (direction == "right") {
-            FL.setPower(1);
-            BL.setPower(1);
-            FR.setPower(-1);
-            BR.setPower(-1);
+            FL.setPower(0.5);
+            BL.setPower(0.5);
+            FR.setPower(-0.5);
+            BR.setPower(-0.5);
         } else if (direction == "left") {
-            FR.setPower(1);
-            BR.setPower(1);
-            FL.setPower(-1);
-            BL.setPower(-1);
+            FR.setPower(0.5);
+            BR.setPower(0.5);
+            FL.setPower(-0.5);
+            BL.setPower(-0.5);
         }
 
         if (tIme != 0) {
@@ -73,10 +73,10 @@ public class autonomousCodeForBlueLeft extends LinearOpMode {
         BL.setTargetPosition(targetToPlace);
         BR.setTargetPosition(targetToPlace);
 
-        FL.setPower(1);
-        FR.setPower(1);
-        BL.setPower(1);
-        BR.setPower(1);
+        FL.setPower(0.5);
+        FR.setPower(0.5);
+        BL.setPower(0.5);
+        BR.setPower(0.5);
 
         FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
