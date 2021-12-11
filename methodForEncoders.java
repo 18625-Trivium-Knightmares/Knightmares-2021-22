98436b047031c86bc59ec5a77abf4ba08316bb9d
Jacøb <85955074+cuber1515@users.noreply.github.com>
2021-12-10 22:21:53 -0700

    Update methodForEncoders.java

--- methodForEncoders.java
+++ methodForEncoders.java
@@ -1,11 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 public class methodForEncoders {
-    public int calculateToPlace(double distance, double ticksPerRev, double diameter) {
-        double circumference = 3.14 * diameter;
+    public int calculateToPlace(double distance) {
+        double circumference = 3.14 * 4.72441;
         double rotationsNeeded = distance * circumference;
-        int encoderTarget = (int) (rotationsNeeded * ticksPerRev);
+        int encoderTarget = (int) (rotationsNeeded * 1338);
         return encoderTarget;
     }
 }
-
