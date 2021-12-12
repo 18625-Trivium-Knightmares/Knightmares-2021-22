dd5b2b8eba90a7c78946b00c19a9ca4c19b1f4f9
Jacøb <85955074+cuber1515@users.noreply.github.com>
2021-12-11 19:06:22 -0700

    This is the code after the match.

--- teleOpCode.java
+++ teleOpCode.java
@@ -8,27 +8,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 
 public class teleOpCode extends LinearOpMode {
-    // wind up
-    public void wind(String upOrDown, int tIme) {
-        if (upOrDown == "up") {
-            RPM.setPower(-0.75);
-            LPM.setPower(-0.75);
-        } else if (upOrDown == "down") {
-            RPM.setPower(0.75);
-            LPM.setPower(0.75);
-        }
-    }
-
-    // duck carousel
-    public void duck(int tIme) {
-        DCM.setPower(-0.5);
-
-        sleep(tIme);
-
-        DCM.setPower(0);
-    }
-
-    DcMotor FR, FL, BR, BL, RPM, LPM, IM, DCM;
+    DcMotor FR, FL, BR, BL, DCM;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -37,20 +17,16 @@ public class teleOpCode extends LinearOpMode {
         FL = hardwareMap.dcMotor.get("Front Left");
         BR = hardwareMap.dcMotor.get("Back Right");
         BL = hardwareMap.dcMotor.get("Back Left");
-        RPM = hardwareMap.dcMotor.get("Right Pulley");
-        LPM = hardwareMap.dcMotor.get("Left Pulley");
-        IM = hardwareMap.dcMotor.get("Intake");
         DCM = hardwareMap.dcMotor.get("Duck");
         FR.setDirection(DcMotor.Direction.REVERSE);
         BR.setDirection(DcMotor.Direction.REVERSE);
-        LPM.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
                 // for speed of robot
-                double speed = 0.5;
+                double speed = 0.7;
 
                 if (gamepad1.dpad_up) {
                     speed += 0.1;
@@ -66,40 +42,16 @@ public class teleOpCode extends LinearOpMode {
                 FL.setPower(gamepad1.left_stick_y * speed);
                 BL.setPower(gamepad1.left_stick_y * speed);
 
-                // pull claw up
-                if (gamepad1.y) {
-                    wind("up", 500);
-                }
-
-                // bring claw down
-                if (gamepad1.a) {
-                    wind("down", 500);
-                }
-
                 if (gamepad1.b) {
-                    DCM.setPower(-1);
+                    DCM.setPower(-0.5);
+                }
+
+                if (gamepad1.right_bumper) {
+                    DCM.setPower(0.5);
                 }
 
                 if (gamepad1.x) {
                     DCM.setPower(0);
-                }
-
-                // pick up
-                if (gamepad1.left_trigger > 0) {
-                    IM.setPower(-0.25);
-                }
-
-                if (gamepad1.left_bumper) {
-                    IM.setPower(0);
-                }
-                
-                // drop
-                if (gamepad1.right_trigger > 0) {
-                    IM.setPower(0.25);
-                }
-                
-                if (gamepad1.right_bumper) {
-                    IM.setPower(0);
                 }
             }
         }
