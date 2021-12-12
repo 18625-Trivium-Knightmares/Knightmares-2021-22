4b679b94baa0d5d8dc4b8615e5cb02d01c626d2d
Jacøb <85955074+cuber1515@users.noreply.github.com>
2021-12-11 19:08:05 -0700

    This is the code after the match.

--- methodForEncoders.java
+++ methodForEncoders.java
@@ -2,9 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 public class methodForEncoders {
     public int calculateToPlace(double distance) {
-        double circumference = 3.14 * 4.72441;
-        double rotationsNeeded = distance * circumference;
+        double circumference = 3.14 * 4.72441; // 4.72441
+        double rotationsNeeded = distance / circumference;
         int encoderTarget = (int) (rotationsNeeded * 1338);
         return encoderTarget;
     }
 }
+
