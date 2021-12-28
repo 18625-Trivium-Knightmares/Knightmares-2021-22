cf23f3002ee7cfce71bc9c6d748d0c971bfcf516
Jacøb <85955074+cuber1515@users.noreply.github.com>
2021-12-27 17:12:00 -0700

    Added controls for arm, claw, and mecanum wheels

--- teleOpCode.java
+++ teleOpCode.java
@@ -8,7 +8,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 
 public class teleOpCode extends LinearOpMode {
-    DcMotor FR, FL, BR, BL, DCM;
+    DcMotor FR, FL, BR, BL, DCM, EJ;
+    Servo CS;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,6 +19,8 @@ public class teleOpCode extends LinearOpMode {
         BR = hardwareMap.dcMotor.get("Back Right");
         BL = hardwareMap.dcMotor.get("Back Left");
         DCM = hardwareMap.dcMotor.get("Duck");
+        EJ = hardwareMap.dcMotor.get("Elbow Joint");
+        CS = hardwareMap.servo.get("Claw");
         FR.setDirection(DcMotor.Direction.REVERSE);
         BR.setDirection(DcMotor.Direction.REVERSE);
 
@@ -32,6 +35,32 @@ public class teleOpCode extends LinearOpMode {
                 FL.setPower(gamepad1.left_stick_y * .5);
                 BL.setPower(gamepad1.left_stick_y * .5);
 
+                if (gamepad1.left_stick_x > 0) {
+                    FR.setPower(gamepad1.left_stick_x * .5);
+                    BL.setPower(gamepad1.left_stick_x * .5);
+                    FL.setPower(-gamepad1.left_stick_x * .5);
+                    BR.setPower(-gamepad1.left_stick_x * .5);
+                    
+                    if (gamepad1.left_stick_x == 0) {
+                        FR.setPower(0);
+                        BR.setPower(0);
+                        BL.setPower(0);
+                        BR.setPower(0);
+                    }
+                } else if (gamepad1.left_stick_x < 0) {
+                    FR.setPower(-gamepad1.left_stick_x * .5);
+                    BL.setPower(-gamepad1.left_stick_x * .5);
+                    FL.setPower(gamepad1.left_stick_x * .5);
+                    BR.setPower(gamepad1.left_stick_x * .5);
+
+                    if (gamepad1.left_stick_x == 0) {
+                        FR.setPower(0);
+                        BR.setPower(0);
+                        BL.setPower(0);
+                        BR.setPower(0);
+                    }
+                }
+
                 if (gamepad1.b) {
                     DCM.setPower(-0.5);
                 }
@@ -42,6 +71,12 @@ public class teleOpCode extends LinearOpMode {
 
                 if (gamepad1.x) {
                     DCM.setPower(0);
+                }
+
+                EJ.setPower(gamepad1.right_stick_x);
+
+                if (gamepad1.a) {
+                    CS.setPosition(5);
                 }
             }
         }
