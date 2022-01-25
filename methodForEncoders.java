1407d126c0e49147c06cc685a9ad54e022f6cfda
Jacøb <85955074+cuber1515@users.noreply.github.com>
2022-01-24 17:59:19 -0700

    added class for measurement by rotation

--- methodForEncoders.java
+++ methodForEncoders.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
 public class methodForEncoders {
-    public int calculateToPlace(double distance) {
+    public int calculateToPlaceDistance(double distance) {
         double circumference = 3.14 * 4.72441; // 4.72441
         double rotationsNeeded = distance / circumference;
         int encoderTarget = (int) (rotationsNeeded * 1338);
+        return encoderTarget;
+    }
+
+    public int calculateToPlaceRotations(double rotations) {
+        int encoderTarget = (int) (rotations * 1120);
         return encoderTarget;
     }
 }
