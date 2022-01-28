119cf7df520994b34210d1f2dbd55c4bf58d6ec7
Jacøb <85955074+cuber1515@users.noreply.github.com>
2022-01-27 17:50:45 -0700

    Fully updated teleOp

--- teleOpCode.java
+++ teleOpCode.java
@@ -34,8 +34,8 @@ public class teleOpCode extends LinearOpMode {
         DCM = hardwareMap.dcMotor.get("Duck");
         AM = hardwareMap.dcMotor.get("Arm Motor");
         CS = hardwareMap.servo.get("Claw");
-        FR.setDirection(DcMotor.Direction.REVERSE);
-        BR.setDirection(DcMotor.Direction.REVERSE);
+        FL.setDirection(DcMotor.Direction.REVERSE);
+        BL.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
 
@@ -44,10 +44,10 @@ public class teleOpCode extends LinearOpMode {
             while (opModeIsActive()) {
 
                 // move robot
-                FR.setPower(gamepad1.right_stick_y * 0.5);
-                BR.setPower(gamepad1.right_stick_y * 0.5);
-                FL.setPower(gamepad1.left_stick_y * 0.5);
-                BL.setPower(gamepad1.left_stick_y * 0.5);
+                FL.setPower(gamepad1.right_stick_y * 0.5);
+                BL.setPower(gamepad1.right_stick_y * 0.5);
+                FR.setPower(gamepad1.left_stick_y * 0.5);
+                BR.setPower(gamepad1.left_stick_y * 0.5);
 
                 if (gamepad1.left_bumper) {
                     DCM.setPower(-1);
@@ -62,13 +62,29 @@ public class teleOpCode extends LinearOpMode {
                 }
 
                 if (gamepad1.right_trigger != 0) {
-                    int rotations = encoders.calculateToPlaceRotations(0.1);
-                    encoders(rotations);
+                    int encoderTarget = (int) (0.75 * 1120);
+                    AM.setTargetPosition(encoderTarget);
+                    AM.setPower(0.4);
+                    AM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//                    while (AM.isBusy()) {
+//                    }
+                    sleep(3000);
+                    AM.setPower(0);
+//                    int rotations = encoders.calculateToPlaceRotations(0.1);
+//                    encoders(rotations);
                 }
 
                 if (gamepad1.left_trigger != 0) {
-                    int rotations = encoders.calculateToPlaceRotations(-0.1);
-                    encoders(rotations);
+                    int encoderTarget = (int) (-0.75 * 1120);
+                    AM.setTargetPosition(encoderTarget);
+                    AM.setPower(0.4);
+                    AM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    sleep(250);
+//                    while (AM.isBusy()) {
+//                    }
+                    AM.setPower(0);
+//                    int rotations = encoders.calculateToPlaceRotations(-0.1);
+//                    encoders(rotations);
                 }
 
                 if (gamepad1.y) {
@@ -76,12 +92,15 @@ public class teleOpCode extends LinearOpMode {
                 }
 
                 if (gamepad1.x) {
-                    CS.setPosition(1);
+                    CS.setPosition(0.02 );
                 }
 
                 if (gamepad1.b) {
-                    CS.setPosition(0.02);
+                    CS.setPosition(1);
                 }
+                telemetry.addData("x is pressed; ", gamepad1.x);
+                telemetry.addData("x is pressed; ", gamepad1.b);
+                telemetry.update();
             }
         }
     }
